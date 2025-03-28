95c5184e3fe745786b6ce53ffaf97a7ef388f081
maria sokolovska <marivso.box@gmail.com>
2025-03-28 18:57:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -357,7 +357,7 @@ function hideSidebarMenu()
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("./sw.js")
+      .register("/sw.js")
       .then(() => console.log("Service Worker registered"))
       .catch((err) => console.error("Service Worker registration failed", err));
 }
